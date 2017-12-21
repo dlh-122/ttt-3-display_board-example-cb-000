e4fe3f9f3863a73a54d3cd73262fd6f4f56c4cf5
Diana Hayes <hayes12288@gmail.com>
2017-12-21 03:11:19 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  cell = "   "
+  middle = "|"
+  lines = "-----------"
+  row = [cell, cell, cell]
+  print [row.join(middle), row.join(middle), row.join(middle)].join(lines)
+  #board = [cell, middle, cell, middle, cell, lines, cell, middle, cell, middle, cell, lines, cell, middle, cell, middle, cell]
+  #puts board
+end
